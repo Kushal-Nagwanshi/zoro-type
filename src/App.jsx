b967d2409d07f685b1359d38875314a85eefc8ee
Kushal Nagwanshi <kushalnagwanshicloud@gmail.com>
2023-04-19 02:54:30 +0530

    Added functionality for moving lines above (when char goes to 3rd line, it resets it to go to the second line)
    Have to fine tune it a little bit, but made it working atleast for now.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import './App.css'
+import {getLineNumber , typingContent , getWidth} from './util' 
 
 let initialTypingContent = ["Typing " , "is " , "fun " , "!! " , "What " , "do " , "You " ,"Guys " , "think?"] ;
-
-let initialCellStates = initialTypingContent.map((word) => {
+initialTypingContent = typingContent ;
+let initialcellStates = initialTypingContent.map((word) => {
   let InitialWordState = [] ; 
   for(let i = 0 ; i < word.length ; i ++){
     InitialWordState.push("letter untyped") ; 
@@ -26,22 +27,23 @@ function isValidKey(key){
   return false;
 }
 
-// to test the CellState behaviour when it will change
+// to test the cellState behaviour when it will change
 /*
-initialCellStates[0][3] = "letter wrong" ;
-initialCellStates[0][1] = "letter typed" ; 
+initialcellStates[0][3] = "letter wrong" ;
+initialcellStates[0][1] = "letter typed" ; 
 */
 function App() {
-  const [currentIndex, setCurrentIndex] = useState([0,0]) 
-  const [typingcontent, setTypingContent] = useState(initialTypingContent) 
-  const [CellState , setCellState] = useState(initialCellStates)
-  
+  let [currentIndex, setCurrentIndex] = useState([0,0]) 
+  let [typingcontent, setTypingContent] = useState(initialTypingContent) 
+  let [cellState , setCellState] = useState(initialcellStates)
+  const refs = useRef(typingContent.map(() => React.createRef())) 
+
   let Type = typingcontent.map((word, idx) => {
     let wordcontainer = [] ; 
     for(let i = 0 ; i < word.length ; i ++){
-      wordcontainer.push( <div className = {i == currentIndex[1] && idx == currentIndex[0] ? CellState[idx][i] + " cursor" : CellState[idx][i] } key = {i}><pre>{word[i]}</pre></div>)
+      wordcontainer.push( <div className = {i == currentIndex[1] && idx == currentIndex[0] ? cellState[idx][i] + " cursor" : cellState[idx][i] } key = {i}><pre>{word[i]}</pre></div>)
     }
-    return (<div className = "word" key = {idx}>{wordcontainer}</div>) 
+    return (<div className = "word" key = {`key${idx}`} ref = {refs.current[idx]} id = {`${idx}`} >{wordcontainer}</div>) 
   })
 
   useEffect(() => {
@@ -49,18 +51,22 @@ function App() {
     function handleKeyDown(event) {
       let i = currentIndex[0] , j = currentIndex[1] ;
       // if( i === typingcontent.length - 1 && j === typingcontent[i].length - 1 ) return ; 
-      // console.log( i , j , event.key) ; 
+      // console.log( i , j , event.key) ;
+      // console.log( refs.current[i] ) ; 
+      let divId = refs.current[i].current.id  ;
+      console.log( getWidth(divId)) ; 
+      
       if (event.key === typingcontent[currentIndex[0]][currentIndex[1]]) {
         // console.log( i , j ) ; 
         // console.log(typingcontent[i].length) ; 
 
-        let newCellState = [...CellState];
-        newCellState[i][j] = "letter correct" ; 
+        let newcellState = [...cellState];
+        newcellState[i][j] = "letter correct" ; 
         
         if( typingcontent[i].length === j + 1 ){ i = i + 1 ; j = 0 ; }
         else j++ ;  
         setCurrentIndex([i,j]) ;
-        setCellState(newCellState) ; 
+        setCellState(newcellState) ; 
         // console.log("typed correctly") ; 
       }
       else if( event.key === 'Backspace'){
@@ -68,33 +74,54 @@ function App() {
         if( j > 0 ) j -=1; 
         else if ( j == 0 && i != 0 ) {i -= 1 ;  j = typingcontent[i].length - 1 ;}
      
-        let newCellState = [...CellState];
-        newCellState[i][j] = "letter untyped" ;
+        let newcellState = [...cellState];
+        newcellState[i][j] = "letter untyped" ;
 
         setCurrentIndex([i,j]) ;
-        setCellState(CellState) ; 
+        setCellState(cellState) ; 
       }
       else if( isValidKey(event.key) ){
-        let newCellState = [...CellState];
-        newCellState[i][j] = "letter wrong" ;
+        let newcellState = [...cellState];
+        newcellState[i][j] = "letter wrong" ;
 
         if( typingcontent[i].length === j + 1 ){ i = i + 1 ; j = 0 ; }
         else j++ ; 
          
         setCurrentIndex([i,j]) ;
-        setCellState(CellState)  ;
+        setCellState(cellState)  ;
       }
+
+      if(j == 0) {
+        // check the current line number.
+        // if line number >= 3 , cut the first line.
+        let lineNumber = getLineNumber( i , getWidth("TA")); 
+        console.log(lineNumber) ; 
+
+        if( lineNumber >= 3 ){
+            let x = 1 , ni = 0 , widthSum = 0 , widthTA = getWidth("TA") ;  
+            while( x < 2 && ni < i ){
+                widthSum += getWidth(`${ni}`) ; 
+                ni++ ; 
+                if( widthSum > widthTA) {x++ ; widthSum = 0 ; }
+            }
+            console.log( `updating content x = ${x} ni = ${ni}`);
+            setCellState( (prevState) => { console.log(prevState) ; return [...prevState].slice(ni) ;}) ; 
+            setTypingContent( (prevState) => {  console.log(prevState) ; return [...prevState].slice(ni) ;}) ; 
+            setCurrentIndex([i - ni,0]) ; 
+        }
+      }
+
     }
     window.addEventListener('keydown', handleKeyDown);
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
-  }, [currentIndex , CellState]);
+  }, [currentIndex , cellState , typingContent]);
 
   return (
   <div className = "TypingAreaContainer">
     <h1>ZoroType</h1>
-    <div className="TypingArea">
+    <div className="TypingArea" id = "TA">
       {Type}  
     </div>
     <button className = "startButton"> Start </button>
