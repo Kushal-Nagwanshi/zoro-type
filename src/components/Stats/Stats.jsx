a6759f579fc2dd582521243aeb2169dab0b0060c
Kushal Nagwanshi <kushalnagwanshicloud@gmail.com>
2023-05-21 19:09:11 +0530

    moved components to components folder

---
+++ src/components/Stats/Stats.jsx
@@ -0,0 +1,47 @@
+import './Stats.css' ; 
+import '../../css/balloon.min.css'
+
+function Stats(props){
+    const totalCorrectWords = props.correct / 5 ; 
+    const totalLetters = props.correct + props.incorrect ; 
+
+    const wpm = props.time === 0 ? 0 : Math.floor( totalCorrectWords / props.time * 60 );
+    const accuracy = totalLetters === 0 ? 0 : Math.floor(props.correct / totalLetters * 100 ) ; 
+    
+    return (
+        <div id="stats">
+            <div id="primary-stats">
+                <div id='wpm'>
+                    <div id="wpm-top">
+                        wpm
+                    </div>
+                    <div id= "wpm-bot">
+                        {wpm} 
+                    </div>
+                </div>
+                <div id="accuracy">
+                    <div id="accuracy-top">
+                        acc
+                    </div>
+                    <div id = "accuracy-bot">
+                        {accuracy} 
+                    </div>
+                </div>
+            </div>
+            <div id="secondary-stats">
+                <div id = 'characters'>
+                    <div id = 'characters-top' >
+                        characters
+                    </div>
+                    <div id="characters-bot" aria-label="correct, incorrect" data-balloon-pos="up">
+                        <div id="correct">{props.correct}</div>
+                        <div id="incorrect">{props.incorrect}</div>
+                    </div>
+                </div>
+            </div>
+           
+        </div>
+    )
+}
+
+export default Stats ; 
---
+++ src/components/TypingArea/TypingArea.jsx
@@ -0,0 +1,192 @@
+import React, { useEffect, useState, useRef } from 'react'
+import './TypingArea.css'
+import {getLineNumber , typingContentZoro , typingContentTypingIsFun, getWidth , isValidKey , isSpecialKey , getRandomWords} from '../../util' 
+import Timer from '../Timer/Timer';
+
+function TypingArea(props) {
+  let [currentIndex, setCurrentIndex] = useState([0,0]) 
+  let [typingContent, setTypingContent] = useState(["R"]) 
+  let [cellState , setCellState] = useState(["letter untyped"])
+  let [restartTest , setRestartTest] = [props.restartTest , props.setRestartTest] 
+  const [isActive , setIsActive] = useState(false) ; 
+  const [isEnded , setIsEnded] = useState(false) ; 
+  const refs = useRef([]) ;
+  const typingAreaRef = useRef();
+
+  let Type = typingContent.map((word, idx) => {
+    let wordcontainer = [] ; 
+    for(let i = 0 ; i < word.length ; i ++){
+      wordcontainer.push( <div className = {i == currentIndex[1] && idx == currentIndex[0] ? cellState[idx][i] + " cursor" : cellState[idx][i] } key = {i}><pre>{word[i]}</pre></div>)
+    }
+    return (<div className = "word" key = {`key${idx}`} ref={el => refs.current[idx] = el} id = {`${idx}`} >{wordcontainer}</div>) 
+  });
+
+  useEffect( () => {
+      typingAreaRef.current.focus() ; 
+  }) ;
+
+  useEffect( () => {
+    let wordList = getRandomWords(100) ;
+    console.log(wordList) ; 
+
+    let initialTypingContent = typingContentTypingIsFun ; 
+    initialTypingContent = typingContentZoro;
+    initialTypingContent = wordList ; 
+    let initialcellStates = initialTypingContent.map((word) => {
+      let InitialWordState = [] ; 
+      for(let i = 0 ; i < word.length ; i ++){
+        InitialWordState.push("letter untyped") ; 
+      }  
+      return InitialWordState ;
+    }) ;
+
+    setTypingContent(initialTypingContent) ; 
+    setCellState(initialcellStates) ;
+
+    setCurrentIndex([0,0]) ;
+  } , [])
+
+  useEffect( () => {
+    if(restartTest){   
+      if (document.activeElement) {
+        document.activeElement.blur();
+        document.body.focus() ; 
+    }
+      console.log("restarting test...") ;
+      let initialTypingContent = typingContentTypingIsFun ; 
+    initialTypingContent = typingContentZoro;
+
+    let initialcellStates = initialTypingContent.map((word) => {
+      let InitialWordState = [] ; 
+      for(let i = 0 ; i < word.length ; i ++){
+        InitialWordState.push("letter untyped") ; 
+      }  
+      return InitialWordState ;
+    }) ;
+      console.log(initialTypingContent);
+      console.log(initialcellStates);
+      
+      setCurrentIndex([0,0]) 
+      setTypingContent(initialTypingContent) 
+      setCellState(initialcellStates)
+      setIsActive(false)
+      setIsEnded(false)
+      setRestartTest(false)
+    }
+    
+
+  } ,[ restartTest , typingContent , cellState , currentIndex]) ; 
+
+ useEffect( () => {
+  if(isEnded) props.setTestEnded(true); 
+ } , [isEnded]) ;
+  
+ useEffect( () => {
+  console.log(props.time) ; 
+ }, [props.time]) ; 
+
+ useEffect(() => {
+
+    function handleKeyDown(event) {
+      let i = currentIndex[0] , j = currentIndex[1] ;
+      let new_i = i , new_j = j ; 
+
+      if( i === 0 && j === 0 ) { 
+        if(isSpecialKey(event.key)) return ; 
+          setIsActive(true) ;
+          props.setTestStarted(true) ; 
+      }
+      // Here run a function which changes page to tell the stats of current typing test..
+      if( i === typingContent.length - 1 && j === typingContent[i].length ) {
+        console.log("Test Ended : setting testEnded to true");
+        props.setTestEnded(true); 
+      }
+
+      // console.log( i , j , event.key) ;
+      // console.log( refs.current[i] ) ; 
+      let divId = refs.current[i].id  ;
+      console.log( getWidth(divId)) ; 
+
+      //correctly typed
+      if (event.key === typingContent[currentIndex[0]][currentIndex[1]]) {
+        // console.log( i , j ) ; 
+        // console.log(typingcontent[i].length) ; 
+        
+        let newcellState = [...cellState];
+        newcellState[i][j] = "letter correct" ; 
+        
+        if( typingContent[i].length === j + 1 ){ new_i = i + 1 ; new_j = 0 ; }
+        else new_j++ ;  
+        setCurrentIndex([new_i,new_j]) ;
+        setCellState(newcellState) ; 
+        props.setCorrect(prev => { console.log(prev) ; return prev+1 ; }) ; 
+        
+        // console.log("typed correctly") ; 
+      }
+      else if( !(i == 0 && j == 0 ) && event.key === 'Backspace'){
+        
+        if( j > 0 ) new_j = j - 1; 
+        else if ( j == 0 && i != 0 ) {new_i = i - 1 ;  new_j = typingContent[new_i].length - 1 ;}
+
+        if(cellState[new_i][new_j] == "letter incorrect") props.setIncorrect(prev => {return prev-1; }) ;
+        else props.setCorrect(prev => {return prev-1;}) ; 
+
+        let newcellState = [...cellState];
+        newcellState[new_i][new_j] = "letter untyped" ;
+        
+        setCurrentIndex([new_i,new_j]) ;
+        setCellState(cellState) ; 
+        return ; 
+      }
+      else if( isValidKey(event.key) ){
+        let newcellState = [...cellState];
+        newcellState[i][j] = "letter wrong" ;
+        
+        if( typingContent[i].length === j + 1 ){ new_i = i + 1 ; new_j = 0 ; }
+        else new_j++ ; 
+         
+        setCurrentIndex([new_i,new_j]) ;
+        setCellState(cellState)  ;
+        props.setIncorrect(prev => {return prev+1 ;}) ; 
+      }
+
+      if(j == typingContent[i].length - 1 && i !== typingContent.length -1 ) {
+        // check the current line number.
+        // if line number >= 3 , cut the first line.
+        let lineNumber = getLineNumber( i + 1 , getWidth("TA")); 
+        console.log(lineNumber) ; 
+
+        if( lineNumber >= 3 ){
+            let x = 1 , ni = 0 , widthSum = 0 , widthTA = getWidth("TA") ;  
+            while( x < 2 && ni < i ){
+                widthSum += getWidth(`${ni}`) ; 
+                ni++ ; 
+                if( widthSum > widthTA) {x++ ; ni--;  widthSum = 0 ; }
+            }
+
+            setCellState( (prevState) => { return [...prevState].slice(ni) ;}) ; 
+            setTypingContent( (prevState) => { return [...prevState].slice(ni) ;}) ; 
+            setCurrentIndex([i - ni + 1,0]) ; 
+        }
+      }
+     
+    }
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [currentIndex , cellState , typingContent]);
+  
+ 
+  return (
+  <div className = "TypingAreaContainer">
+    { isActive && <Timer seconds = {props.time} setIsEnded = {setIsEnded} isActive = {isActive} setIsActive = {setIsActive}/> }
+    <div className="TypingArea"  tabIndex={-1}  ref ={typingAreaRef} id = "TA">
+      {Type}  
+    </div>
+  </div>
+   
+  )
+}
+
+export default TypingArea
